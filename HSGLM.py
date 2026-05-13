64b42fc15b490ae9ddfbe5fd94cced6cbe4f74c0
XL-Jiang <jxl0ccc@163.com>
2026-05-13 14:28:17 +0800

    Update and rename LG_MDGL.py to HSGLM.py

--- HSGLM.py
+++ HSGLM.py
@@ -1,17 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul  6 17:25:00 2022
-
-@author: 86178
-"""
 from Module import *
-from L_MDGL import L_MDGL
-from G_MDGL import G_MDGL
+from L_SGL import L_SGL
+from G_SGL import G_SGL
 
-#### Local to  Global Multi-scale Fusion and Classification
-class LG_MDGL(nn.Module):
+#### Hierarchical spatiotemporal graph learning method with Mamba
+class HSGLM(nn.Module):
     def __init__(self, input_dim1,input_dim2, input_dim3,hidden_dim,topk, phd_dim,num_classes,num_heads, num_time,window_size, kernel_size,cls_token='max', readout='baro'):
-        super(LG_MDGL, self).__init__()
+        super(HSGLM, self).__init__()
         assert cls_token in ['sum', 'mean','max', 'param']
         if cls_token == 'sum':
             self.cls_token = lambda x: x.sum(1)
@@ -24,11 +18,11 @@ class LG_MDGL(nn.Module):
         else:
             raise ValueError("Invalid cls_token type")
         #Local Multi-scale Dynamic Graph Learning
-        self.L_MDGL1= L_MDGL(hidden_dim,num_time,window_size, kernel_size)
-        self.L_MDGL2 = L_MDGL(hidden_dim, num_time, window_size, kernel_size)
-        self.L_MDGL3 = L_MDGL(hidden_dim, num_time, window_size, kernel_size)
+        self.L_SGL1= L_SGL(hidden_dim,num_time,window_size, kernel_size)
+        self.L_SGL2 = L_SGL(hidden_dim, num_time, window_size, kernel_size)
+        self.L_SGL3 = L_SGL(hidden_dim, num_time, window_size, kernel_size)
         # Global Multi-scale Dynamic Graph Learning
-        self.G_MDGL= G_MDGL(input_dim1,input_dim2,input_dim3, hidden_dim,topk, window_size,num_heads, num_time,cls_token=cls_token, readout=readout)
+        self.G_SGL= G_SGL(input_dim1,input_dim2,input_dim3, hidden_dim,topk, window_size,num_heads, num_time,cls_token=cls_token, readout=readout)
         #Non-imaging Data Processing
         self.mlp = nn.Sequential(nn.BatchNorm1d(phd_dim),nn.Linear(phd_dim, hidden_dim), nn.ReLU(),nn.Linear(hidden_dim, hidden_dim), nn.BatchNorm1d(hidden_dim), nn.ReLU())
         # Classfier
@@ -50,12 +44,12 @@ class LG_MDGL(nn.Module):
         minibatch_size, num_windows, num_timepoints,num_nodes1 = dtimesreies1.shape[:4]
         minibatch_size, num_windows, num_timepoints, num_nodes2 = dtimesreies2.shape[:4]
         minibatch_size, num_windows, num_timepoints, num_nodes3 = dtimesreies3.shape[:4]
-        #L_MDGL
-        dy_local1,dy_adjs1 = self.L_MDGL1(dtimesreies1) #b w n1 t,(b w) n1 n1
-        dy_local2, dy_adjs2 = self.L_MDGL2(dtimesreies2)  # b w n2 t,(b w) n2 n2
-        dy_local3, dy_adjs3 = self.L_MDGL2(dtimesreies3)  # b w n3 t,(b w) n3 n3
-        #G_MDGL
-        dy_mambaout,attention = self.G_MDGL(dy_local1,dy_local2,dy_local3,dy_adjs1,dy_adjs2,dy_adjs3) #b hidden_dim
+        #L_SGL
+        dy_local1,dy_adjs1 = self.L_SGL1(dtimesreies1) #b w n1 t,(b w) n1 n1
+        dy_local2, dy_adjs2 = self.L_SGL2(dtimesreies2)  # b w n2 t,(b w) n2 n2
+        dy_local3, dy_adjs3 = self.L_SGL2(dtimesreies3)  # b w n3 t,(b w) n3 n3
+        #G_SGL
+        dy_mambaout,attention = self.G_SGL(dy_local1,dy_local2,dy_local3,dy_adjs1,dy_adjs2,dy_adjs3) #b hidden_dim
         #process Non-imaging Data
         phd_out = self.mlp(phd_ftrs) #b hidden_dim
         #cat fMRI & phd
